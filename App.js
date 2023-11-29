026bcc3b628de5b267ab2f70cad6e76d590468c8
morongoaabigail <morongoaabigail204@gmail.com>
2023-11-29 12:34:40 +0200

    added location

--- App.js
+++ App.js
@@ -6,11 +6,14 @@ import Home from "./src/pages/Home";
 import Login from "./src/pages/Login";
 import Signup from "./src/pages/Signup";
 import Search from "./src/pages/Search";
+import Location from "./src/pages/Location"
+
+
 export default function App() {
   const Stack = createNativeStackNavigator();
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator initialRouteName="Location">
         <Stack.Screen
           name="Home"
           component={Home}
@@ -19,6 +22,7 @@ export default function App() {
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Signup" component={Signup} />
         <Stack.Screen name="Search" component={Search} />
+        <Stack.Screen name="Location" component={Location} options={{ headerShown: false }}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/pages/Location.js
+++ src/pages/Location.js
@@ -1,10 +1,94 @@
-import { View, Text } from "react-native";
-import React from "react";
+import React, { useState } from "react";
+import {
+  View,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  Text,
+  Alert,
+} from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+import axios from 'axios';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 
-export default function Location() {
+
+import { Entypo } from '@expo/vector-icons'; 
+
+const SearchBar = () => {
+  const [searchText, setSearchText] = useState("");
+  const [location, setLocation] = useState(null);
+  
+
+  const handleSearch = () => {
+    console.log("Searching for:", searchText);
+  };
+
   return (
-    <View>
-      <Text>Location</Text>
+    <View style={styles.MainContainer}>
+      <Text
+        style={{
+          marginTop: "20%",
+          fontWeight: "800",
+          fontSize: 24,
+          marginLeft: "8%",
+        }}
+      >
+        Add area
+      </Text>
+      <View style={styles.container}>
+        <TouchableOpacity style={styles.iconContainer} onPress={handleSearch}>
+          <AntDesign name="search1" size={16} color="black" />
+        </TouchableOpacity>
+
+        <TextInput
+          style={styles.input}
+          placeholder="Search for area"
+          placeholderTextColor="black"
+          color="black"
+          value={searchText}
+          onChangeText={(text) => setSearchText(text)}
+          onPress={(data, details = null) => {
+            console.log(data, details);
+          }}
+          query={{
+            key: 'YOUR API KEY',
+            language: 'en',
+          }}
+        />
+      </View>
+
+      <Entypo name="location-pin" size={40} color="#176B87"  alignSelf="center"
+    alignItems= "center" marginTop={50} />
     </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  MainContainer: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+  container: {
+    flexDirection: "row",
+    alignSelf: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+    borderRadius: 8,
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    marginTop: "12%",
+    height: "8%",
+    width: "85%",
+    borderColor: "black",
+    borderWidth: 1,
+  },
+  input: {
+    flex: 1,
+    color: "white",
+  },
+  iconContainer: {
+    marginRight: 12,
+  },
+});
+
+export default SearchBar;
